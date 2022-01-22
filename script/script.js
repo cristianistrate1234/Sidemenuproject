a8def8aa75112503932474816ecacc17e6134480
Cristi2002 <cristianistrate1234@gmail.com>
2022-01-22 18:20:27 +0300

    Am creat proiect

--- script/script.js
+++ script/script.js
@@ -10,4 +10,61 @@ function closeFunction(){
     document.getElementById("mainbox").innerHTML = "&#9776;";
 }
 
-console.log("test");
+const mainbox = document.querySelector("#mainbox");
+
+mainbox.addEventListener("click" , ()=>{
+    setTimeout(function(){
+        document.querySelector('.test').style.visibility = "visible";
+    },200);
+    setTimeout(function(){
+        document.querySelector('.test2').style.visibility = "visible";
+    },350);
+    setTimeout(function(){
+        document.querySelector('.test3').style.visibility = "visible";
+    },500);
+    setTimeout(function(){
+        document.querySelector('.test4').style.visibility = "visible";
+    },650);
+    setTimeout(function(){
+        document.querySelector('.test5').style.visibility = "visible";
+    },800);
+    setTimeout(function(){
+        document.querySelector('.test6').style.visibility = "visible";
+    },950);
+    setTimeout(function(){
+        document.querySelector('.test7').style.visibility = "visible";
+    },1100);
+    setTimeout(function(){
+        document.querySelector('.test8').style.visibility = "visible";
+    },1250);
+
+})
+
+const closebtn = document.querySelector(".closebtn");
+
+closebtn.addEventListener("click" , () =>{
+    setTimeout(function(){
+        document.querySelector('.test').style.visibility = "hidden";
+    },100);
+    setTimeout(function(){
+        document.querySelector('.test2').style.visibility = "hidden";
+    },100);
+    setTimeout(function(){
+        document.querySelector('.test3').style.visibility = "hidden";
+    },100);
+    setTimeout(function(){
+        document.querySelector('.test4').style.visibility = "hidden";
+    },100);
+    setTimeout(function(){
+        document.querySelector('.test5').style.visibility = "hidden";
+    },100);
+    setTimeout(function(){
+        document.querySelector('.test6').style.visibility = "hidden";
+    },100);
+    setTimeout(function(){
+        document.querySelector('.test7').style.visibility = "hidden";
+    },100);
+    setTimeout(function(){
+        document.querySelector('.test8').style.visibility = "hidden";
+    },100);
+})
